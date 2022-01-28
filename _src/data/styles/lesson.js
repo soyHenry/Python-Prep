ac85fa55f502c7433f8841537a59fd5218fa0a50
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-01-28 15:52:22 -0300

    CloudFront implementation

--- _src/data/styles/lesson.js
+++ _src/data/styles/lesson.js
@@ -1,7 +1,7 @@
 const Cache = require('@11ty/eleventy-cache-assets')
 
 module.exports = async function () {
-  const url = 'https://henry-11ty-resources.s3.sa-east-1.amazonaws.com/Styles/lesson.css'
+  const url = 'https://d31uz8lwfmyn8g.cloudfront.net/Styles/lesson.css'
   const style = await Cache(url, {
     duration: '1d',
     type: 'txt',
