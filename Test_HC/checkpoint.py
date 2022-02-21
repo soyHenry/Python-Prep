38b7d3b913cacb5c00f803c812222e879504e39a
lopezdar222 <41219954+lopezdar222@users.noreply.github.com>
2022-02-21 16:24:51 -0300

    hc

--- Test_HC/checkpoint.py
+++ Test_HC/checkpoint.py
@@ -28,6 +28,23 @@ def DividirDosNumeros(dividendo, divisor):
     #Tu código aca:
     return parte_entera, resto
 
+def NumeroCapicua(numero):
+    '''
+    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
+    se refiere a cualquier número que se lee igual de izquierda a derecha que 
+    de derecha a izquierda. Se denominan también números palíndromos.
+    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
+    devuelve el valor booleano False 
+    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
+    Recibe un argumento:
+        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
+    Ej:
+        NumeroCapicua(787) debe retornar True
+        NumeroCapicua(108) debe retornar False
+    '''
+    #Tu código aca:
+    return -1
+
 def Factorial(numero):
     '''
     Esta función devuelve el factorial del número pasado como parámetro.
@@ -39,8 +56,7 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return None
-
+    
 def ProximoPrimo(actual_primo):
     '''
     Esta función devuelve el número primo siguiente al enviado como parámetro.
@@ -52,21 +68,54 @@ def ProximoPrimo(actual_primo):
         ProximoPrimo(8) debe retornar nulo
     '''
     #Tu código aca:
-    return None
+    return 0
 
-def NumeroCapicua(numero):
+def factorizar_numero(numero):
     '''
-    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
-    se refiere a cualquier número que se lee igual de izquierda a derecha que 
-    de derecha a izquierda.
-    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
-    devuelve el valor booleano False 
-    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
+    Esta función recibe como parámetro un número entero mayor a cero y devuelva dos listas, 
+    una con cada factor común y otra con su exponente, 
+    esas dos listas tienen que estar contenidas en otra lista.
+    En caso de que el parámetro no sea de tipo entero y/ó mayor a cero debe retornar nulo.
     Recibe un argumento:
-        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
+        numero: Será el número sobre el que se hará la factorización.
     Ej:
-        NumeroCapicua(787) debe retornar True
-        NumeroCapicua(108) debe retornar False
+
+        factorizar_numero(12) debe retornar [[2,3],[2,1]]
+        factorizar_numero(13) debe retornar [[13],[1]]
+        factorizar_numero(14) debe retornar [[2,7],[1,1]]
     '''
     #Tu código aca:
-    return None
+    return 0
+
+def ClaseAnimal(especie, color):
+    '''
+    Esta función devuelve un objeto instanciado de la clase Animal, 
+    la cual debe tener los siguientes atributos:
+        Edad    (Un valor de tipo de dato entero, que debe inicializarse en cero)
+        Especie (Un valor de tipo de dato string)
+        Color   (Un valor de tipo de dato string)
+    y debe tener el siguiente método:
+        CumplirAnios  (este método debe sumar uno al atributo Edad y debe devolver ese valor)
+    Recibe tres argumento:
+        especie: Dato que se asignará al atributo Especie del objeto de la clase Animal
+        color: Dato que se asignará al atributo Color del objeto de la clase Animal
+    Ej:
+        a = ClaseAnimal('perro','blanco')
+        a.CumpliAnios() -> debe devolver 1
+        a.CumpliAnios() -> debe devolver 2
+        a.CumpliAnios() -> debe devolver 3
+    '''
+
+def NumeroBinario(numero):
+    '''
+    Esta función recibe como parámetro un número entero mayor ó igual a cero y lo devuelve en su 
+    representación binaria. Debe recibir y devolver un valor de tipo entero.
+    En caso de que el parámetro no sea de tipo entero y mayor a -1 debe retornar nulo.
+    Recibe un argumento:
+        numero: Será el número que se convertirá a binario.
+    Ej:
+        NumeroBinario(12) debe retornar 1100
+        NumeroBinario(2) debe retornar 10
+        NumeroBinario(14) debe retornar 1110
+    '''
+    return 0
--- Test_HC/tests.py
+++ Test_HC/tests.py
@@ -1,17 +1,160 @@
 import unittest
 import os
-import checkpoint_ejemplo as ch
+import checkpoint as ch
 
 class HenryChallenge(unittest.TestCase):
     
-    def test_Func_Ejemplo_1(self):
-      valor_test = ch.Func_Ejemplo(1,2)
+    def test_ListaEnteros_01(self):
+      lista_test = ch.ListaEnteros(1, 10)
+      lista_esperada = [1,2,3,4,5,6,7,8,9,10]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_ListaEnteros_02(self):
+      lista_test = ch.ListaEnteros(3, 7)
+      lista_esperada = [3,4,5,6,7,8,9,10]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_ListaEnteros_03(self):
+      lista_test = ch.ListaEnteros(-2, 4)
+      lista_esperada = [-2,-1,0,1]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_DividirDosNumeros_01(self):
+      parte_entera, resto = ch.DividirDosNumeros(10, 5)
+      lista_test = [parte_entera, resto]
+      lista_esperada = [2,0]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_DividirDosNumeros_02(self):
+      parte_entera, resto = ch.DividirDosNumeros(17, 3)
+      lista_test = [parte_entera, resto]
+      lista_esperada = [5,2]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_DividirDosNumeros_03(self):
+      parte_entera, resto = ch.DividirDosNumeros(13, 3)
+      lista_test = [parte_entera, resto]
+      lista_esperada = [4,1]
+      self.assertEqual(lista_test, lista_esperada)
+
+    def test_NumeroCapicua_01(self):
+      valor_test = ch.NumeroCapicua(4589)
+      valor_esperado = False
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroCapicua_02(self):
+      valor_test = ch.NumeroCapicua(92529)
+      valor_esperado = True
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroCapicua_03(self):
+      valor_test = ch.NumeroCapicua('hola')
+      valor_esperado = None
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroCapicua_04(self):
+      valor_test = ch.NumeroCapicua(3333)
+      valor_esperado = True
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_Factorial_01(self):
+      valor_test = ch.Factorial(5)
+      valor_esperado = 120
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_Factorial_02(self):
+      valor_test = ch.Factorial(1)
       valor_esperado = 1
       self.assertEqual(valor_test, valor_esperado)
+
+    def test_Factorial_03(self):
+      valor_test = ch.Factorial(0)
+      valor_esperado = None
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_Factorial_03(self):
+      valor_test = ch.Factorial(10)
+      valor_esperado = 3628800
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ProximoPrimo_01(self):
+      valor_test = ch.ProximoPrimo(5)
+      valor_esperado = 7
+      self.assertEqual(valor_test, valor_esperado)
       
-    def test_Func_Ejemplo_2(self):
-      valor_test = ch.Func_Ejemplo(1,2)
+    def test_ProximoPrimo_02(self):
+      valor_test = ch.ProximoPrimo(61)
+      valor_esperado = 67
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ProximoPrimo_03(self):
+      valor_test = ch.ProximoPrimo(139)
+      valor_esperado = 149
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ProximoPrimo_04(self):
+      valor_test = ch.ProximoPrimo(200)
+      valor_esperado = None
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_factorizar_numero_01(self):
+      valor_test = ch.factorizar_numero(5)
+      valor_esperado = [[5],[1]]
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_factorizar_numero_02(self):
+      valor_test = ch.factorizar_numero(1428)
+      valor_esperado = [[2,3,7,17], [2,1,1,1]]
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_factorizar_numero_01(self):
+      valor_test = ch.factorizar_numero('cinco')
+      valor_esperado = None
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ClaseAnimal_01(self):
+      a = ch.ClaseAnimal('perro','negro')
+      valor_test = a.CumplirAnios()
+      valor_test = a.CumplirAnios()
+      valor_test = a.CumplirAnios()
+      valor_esperado = 3
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ClaseAnimal_02(self):
+      a = ch.ClaseAnimal('ballena','azul')
+      for i in range(0,10):
+        valor_test = a.CumplirAnios()
+      valor_esperado = 10
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_ClaseAnimal_03(self):
+      a = ch.ClaseAnimal('tortuga','verde')
+      for i in range(0,100):
+        valor_test = a.CumplirAnios()
+      valor_esperado = 100
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroBinario_01(self):
+      valor_test = ch.NumeroBinario(5)
+      valor_esperado = 101
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroBinario_02(self):
+      valor_test = ch.NumeroBinario(255)
+      valor_esperado = 11111111
+      self.assertEqual(valor_test, valor_esperado)
+
+    def test_NumeroBinario_03(self):
+      valor_test = ch.NumeroBinario(-10)
       valor_esperado = None
       self.assertEqual(valor_test, valor_esperado)
 
 resultado_test = unittest.main(argv=[''], verbosity=2, exit=False)
+archivo_test = open('resultado_test.csv', 'w')
+archivo_test.write('Total_Tests,Total_Fallas,Total_Errores\n')
+archivo_test.write(str(resultado_test.result.testsRun)+','+str(len(resultado_test.result.failures))+','+str(len(resultado_test.result.errors))+'\n')
+archivo_test.close()
+print('Resumen')
+print('Total Tests:', str(resultado_test.result.testsRun))
+print('Total Fallas:', str(len(resultado_test.result.failures)))
+print('Total Errores:', str(len(resultado_test.result.errors)))
